1b91d5a79717102adc3e5b4637b4cb6fc12c607d
satya <satyaswarup98@gmail.com>
2020-12-11 23:50:03 +0530

    v1.3

--- app/src/main/java/com/example/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/quakereport/EarthquakeActivity.java
@@ -4,25 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    ListView earthquakeListView;
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=2020-01-05&minmagnitude=2.9";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
+           earthquakeListView = (ListView) findViewById(R.id.list);
 
-            ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-            ListView earthquakeListView = (ListView) findViewById(R.id.list);
-            EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
-            earthquakeListView.setAdapter(adapter);
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute();
         }
+
+    private void updateUi(ArrayList<Earthquake> earthquake) {
+        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquake);
+        earthquakeListView.setAdapter(adapter);
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<URL, Void, ArrayList<Earthquake>> {
+
+        @Override
+        protected ArrayList<Earthquake> doInBackground(URL... urls) {
+            return QueryUtils.fetchEarthquakeData(USGS_REQUEST_URL);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquake) {
+            if (earthquake == null) {
+                return;
+            }
+            updateUi(earthquake);
+        }
+
+    }
+
 
 }
